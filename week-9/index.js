d79537d0f3945939c5ae60bc6bbd63dab89c27f7
Aayush Rai <np03cs4a220217@heraldcollege.edu.np>
2023-01-19 00:17:29 +0545

    web technology workshop

---
+++ week-9/index.js
@@ -0,0 +1,51 @@
+//console.log("Hello World I am learning Javascript");
+
+/*let a= 6;
+let b= 8;
+let sum= a+b;
+console.log(sum);*/
+
+/*let a;
+var b;
+const pi = 3.41;
+console.log(pi);*/
+
+/*const PI =3.14;
+let radius = window.prompt("enter a radius")
+console.log(radius);
+let area = PI * radius * radius;
+console.log(area);
+alert(area);*/
+
+/*let name="Roshan";
+let age = window.prompt("enter the age");
+let bread="human";
+console.log(age);
+function sleep(){
+if(age>=10){
+    alert(name+ " the dog is very old" + "he is of " +bread)
+}else{
+    alert(name+ " the dog is very young" + "he is of" +bread)
+}
+}
+function beark(){
+    console.log(age);
+}
+
+sleep();
+beark();*/
+
+/*document.getElementById("myBtn").onclick = function(){
+    let radius = document.getElementById("myNumber").value;
+    console.log(radius);
+    const PI = 3.14;
+    let circumference = 2 * PI * radius;
+    //document.getElementById("my lable").innerHTML =circumference;
+    document.getElementById("circumference").innerHTML ="the circumference of a ciecle" + circumference;
+}*/
+
+document.getElementById("myBtn").onclick = function(){
+    let string = document.getElementById("myString").value;
+    document.getElementById("User").innerHTML ="the user" + user;
+}
+
