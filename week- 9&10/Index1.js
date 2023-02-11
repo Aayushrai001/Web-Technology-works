6a2a598e86303f867e550b892b8d5b8a52fc7f28
Aayush Rai <arai03178@gmail.com>
2023-02-11 10:30:34 +0545

    Week-12 task

---
+++ week- 9&10/Index1.js
@@ -0,0 +1,19 @@
+/*let country = 'Portugal';
+let continent = 'Europe';
+let population = 10;
+console.log(country);
+console.log(continent);
+console.log(population);*/
+
+let isIsland = false;
+let language = none;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+/*language='portuguese';
+const country='portugal';
+const continent='europe';
+const isIsland=false;
+isIsland=true;*/
